d5dffcbd500ec88844f0e7f439410a51df32d5f4
gerardoojeda47 <gerardozapatos@gmail.com>
2025-04-23 17:31:24 -0500

    cambio3

--- evaluacion-python-cesar/app.py
+++ evaluacion-python-cesar/app.py
@@ -41,3 +41,5 @@ from routers.lasguias import *
 
 if __name__ == "__main__":
     app.run(port=6510, host="0.0.0.0", debug=True)
+    
+    # Gerardo: No additional code is needed here as the application is already set to run.
